437b656a6bd28b6d02bf116a519589e78741ab1b
Artur <arturfulgonixavier@gmail.com>
2017-10-12 05:26:39 -0300

    Criação do servlet Login.

---
+++ src/java/servlets/Login.java
@@ -0,0 +1,20 @@
+package servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+@WebServlet(name = "Login", urlPatterns = {"/Login"})
+public class Login extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        HttpSession session = request.getSession();
+        String nome = request.getParameter("txtNome");
+        session.setAttribute("nome", nome);
+        response.sendRedirect(request.getContextPath() + "/home.jsp");
+    }
+}
